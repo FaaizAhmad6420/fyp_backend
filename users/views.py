ae2848d6bd6d25558d64dbc871b5e5d7835f5a02
Faaiz Ahmad <faizahmad6420@gmail.com>
2026-01-02 02:17:59 +0500

    Authentication System

---
+++ users/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+# Create your views here.
+class RegisterView(APIView):
+    permission_classes = []  # public API
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "User registered successfully"},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email
+        })
